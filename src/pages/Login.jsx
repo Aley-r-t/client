f299cab5f2ba85316d72aedc4984fa1836da4fdc
Contreras Perales Rodrigo <levicom134@gmail.com>
2024-11-27 10:30:10 -0500

    change login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -51,55 +51,53 @@ const Login = ({ setAuthToken }) => {
   };
 
   return (
-
     <MainLayout>
       <div
-        className="min-h-screen flex justify-center items-center bg-cover bg-center"
+        className="min-h-screen flex flex-col justify-center items-center bg-cover bg-center"
         style={{ backgroundImage: "url('/fondo.jpg')" }}
-      ></div>
-      <div className="bg-white bg-opacity-50 p-8 rounded-lg shadow-lg w-full max-w-sm">
-        <h2 className="text-3xl font-semibold text-center text-gray-800 mb-6">Iniciar Sesión</h2>
-
-        {/* Mostrar mensaje de error si existe */}
-        {errorMessage && (
-          <div className="bg-red-500 text-white p-3 rounded-md mb-4 text-center">
-            {errorMessage}
-          </div>
-        )}
-
-        <form onSubmit={handleLogin}>
-          <div className="relative mb-4">
-            <input
-              type="text"
-              placeholder="Usuario"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="w-full py-3 pl-10 pr-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-lg"
-            />
-            <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500">👤</span>
-          </div>
-          <div className="relative mb-6">
-            <input
-              type="password"
-              placeholder="Contraseña"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full py-3 pl-10 pr-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-lg"
-            />
-            <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500">🔒</span>
-          </div>
-          <button
-            type="submit"
-            className="w-full py-3 bg-teal-600 text-white text-lg font-semibold rounded-md hover:bg-teal-700 transition-colors"
-          >
-            Ingresar
-          </button>
-        </form>
+      >
+        <div className="bg-white bg-opacity-50 p-8 rounded-lg shadow-lg w-full max-w-sm">
+          <h2 className="text-3xl font-semibold text-center text-gray-800 mb-6">Iniciar Sesión</h2>
+  
+          {/* Mostrar mensaje de error si existe */}
+          {errorMessage && (
+            <div className="bg-red-500 text-white p-3 rounded-md mb-4 text-center">
+              {errorMessage}
+            </div>
+          )}
+  
+          <form onSubmit={handleLogin}>
+            <div className="relative mb-4">
+              <input
+                type="text"
+                placeholder="Usuario"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="w-full py-3 pl-10 pr-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-lg"
+              />
+              <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500">👤</span>
+            </div>
+            <div className="relative mb-6">
+              <input
+                type="password"
+                placeholder="Contraseña"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full py-3 pl-10 pr-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-lg"
+              />
+              <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500">🔒</span>
+            </div>
+            <button
+              type="submit"
+              className="w-full py-3 bg-teal-600 text-white text-lg font-semibold rounded-md hover:bg-teal-700 transition-colors"
+            >
+              Ingresar
+            </button>
+          </form>
+        </div>
       </div>
-
     </MainLayout>
-
-  );
+  ); 
 };
 
 export default Login;
